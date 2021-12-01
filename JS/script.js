29ccf823eb98213027838581909dfdcd0e5b1a68
Alejandro Santiago <cubanfireball81@gmail.com>
2021-11-30 21:49:11 -0900

    Breakpoints added and modified

--- JS/script.js
+++ JS/script.js
@@ -11,10 +11,7 @@ async function getNews () {
     return posts;
 }
 
-
-
-
-
+/*Use Data from API to Populate Cards in the DOM*/
 function newsToPage (posts) {
     posts.forEach(post => {
 
@@ -53,28 +50,18 @@ function newsToPage (posts) {
 
 getNews().then(posts => newsToPage(posts));
 
-/*Subscribe Modal*/
-
-// Get the modal
+/*Subscribe Modal and it's Functionality*/
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
     modal.style.display = "none";
